594dcaaf83f7df8075dea84622d8d6945738c315
DiskKun <luca5h1905@gmail.com>
2025-12-22 14:49:23 -0500

    dragon stuff

---
+++ Assets/Scripts/Dragon.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using System;
+using Unity;
+
+public class Dragon : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Reference to the CameraManager")]
+    CameraManager cameraManager;
+    [SerializeField]
+    [Tooltip("Particle System Reference")]
+    ParticleSystem sleepParticles;
+    [SerializeField]
+    [Tooltip("Reference to the sleeping alert Icon")]
+    GameObject alertIcon;
+    [SerializeField]
+    [Tooltip("The number of seconds that can be spent in the zone by the player(s) before the dragon wakes up")]
+    float timeToWake = 10;
+    [SerializeField]
+    [Tooltip("Whether or not the timer goes into double-time when two players are in the area")]
+    bool stackPlayerTime = false;
+    [SerializeField]
+    [Tooltip("The amount of time in seconds that the player has spent in the dragon's zone")]
+    float secondsInZone = 0;
+
+    int playersInArea;
+    bool asleep = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (asleep)
+        {
+            if (stackPlayerTime)
+            {
+                secondsInZone += Time.deltaTime * playersInArea;
+            }
+            else if (playersInArea > 0)
+            {
+                secondsInZone += Time.deltaTime;
+            }
+            if (playersInArea == 0)
+            {
+                secondsInZone -= Time.deltaTime;
+            }
+            secondsInZone = Mathf.Clamp(secondsInZone, 0, timeToWake);
+        }
+        if (secondsInZone == 0 && !sleepParticles.isPlaying)
+        {
+            sleepParticles.Play();
+            alertIcon.SetActive(false);
+        }
+        if (secondsInZone > 0 && sleepParticles.isPlaying)
+        {
+            sleepParticles.Stop();
+            alertIcon.SetActive(true);
+        }
+        if (secondsInZone == timeToWake)
+        {
+            DragonWake();
+        }
+            
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            playersInArea += 1;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            playersInArea -= 1;
+    }
+
+
+
+
+    void DragonWake()
+    {
+        asleep = false;
+        alertIcon.SetActive(false);
+        cameraManager.camIndex = 2;
+    }
+}
